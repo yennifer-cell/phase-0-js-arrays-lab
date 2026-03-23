f7e97bb74ba625aab5d2b6aa5b7360467357a414
yennifer <shiksjayne95@gmail.com>
2026-03-23 14:01:29 +0300

    Refactor inventory management module exports

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -23,11 +23,3 @@ module.exports = {
   removeLastProduct,
   products
 };
-// Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct: undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct: undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName: undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct: undefined,
-  products
-};
